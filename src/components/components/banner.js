0ae086cf86631a890b9e7f533e7678264f888388
Gentlesammy <samodunlade84@gmail.com>
2021-03-03 09:26:08 +0100

    banner movie set

--- src/components/components/banner.js
+++ src/components/components/banner.js
@@ -1,14 +1,39 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "../styles/Banner.css";
+import axios from "../../hooks/axios";
+import links from "../../hooks/links";
 
 const Banner = () => {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    const getMovie = async () => {
+      const request = await axios.get(links.fetchTrending);
+      setMovies(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+    };
+    getMovie();
+  }, []);
+
+  console.log(movies);
   const truuncateDesc = (string, n) => {
     return string?.length > n ? string.substr(0, n - 1) + "..." : string;
   };
   return (
-    <div className="banner_section">
+    <div
+      className="banner_section"
+      style={{
+        background: `linear-gradient(0deg, rgba(12, 1, 7, 0.3), rgba(12, 1, 7, 0.3)), url("https://image.tmdb.org/t/p/original/${
+          movies ? movies.backdrop_path : ""
+        }")`,
+        backgroundSize: "cover !important",
+        backgroundPosition: "center center !important",
+      }}
+    >
       <div className="banner_content">
-        <h1 className="banner_title"> Moonlight Tale</h1>
+        <h1 className="banner_title"> {movies ? movies.original_title : ""}</h1>
         <div className="banner_buttons">
           <button className="play_buttons banner_button"> Play </button>
           <button className="playlist_buttons banner_button">
@@ -17,10 +42,7 @@ const Banner = () => {
           </button>
         </div>
         <p className="banner_description">
-          {truuncateDesc(
-            "In the midst of darkness and calamity, a young soul arose... ",
-            100
-          )}
+          {truuncateDesc(movies ? movies.overview : "", 150)}
         </p>
       </div>
     </div>
