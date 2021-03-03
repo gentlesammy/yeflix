de29f4446efe799765e3b83cecd38cb7ef8fb329
Gentlesammy <samodunlade84@gmail.com>
2021-03-03 07:14:40 +0100

    api get request links created

--- src/hooks/links.js
+++ src/hooks/links.js
@@ -1,1 +1,13 @@
-const api_keys = process.env.REACT_APP_TMDB_API_KEY;
+const api_key = process.env.REACT_APP_TMDB_API_KEY;
+const links = {
+  fetchTrending: `/trending/all/week?api_key=${api_key}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${api_key}&with_networks=213`,
+  fetchTopRtaed: `/movie/top_rated?api_key=${api_key}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${api_key}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${api_key}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${api_key}&with_genres=27`,
+  fetchRomanticMovies: `/discover/movie?api_key=${api_key}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${api_key}&with_genres=99`,
+};
+
+export default links;
